12d9a6b0a57136ec9663f25a691d2ae1820d3544
jatinhasijabluesven <34453887+jatinhasijabluesven@users.noreply.github.com>
2017-12-11 21:52:04 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ from process_data import *
 
 app = Flask(__name__)
 
+"""
+Load Home Page from template/home.html
+"""
 @app.route('/')
 def home():
 	return render_template('home.html')
 
+"""
+Load Search result Page from template/search.html
+"""
 @app.route('/search',methods = ['POST'])
 def search():
-	print('hello')
 	station_name = request.form.get('station_name')
 	start_time = request.form.get('start_time')
 	end_time = request.form.get('end_time')
-	text = func(station_name , start_time , end_time)
-	print(type(text))
-	return render_template('search.html', html = text)
+	#function to retrieve the search results
+	text,count = func(station_name , start_time , end_time)
+	#render the data to prepare search results
+	return render_template('search.html', html = '<div> Number of trains between interval is ' + str(count) + '</div>' + text) 
 
 if __name__ == '__main__':
-	app.run(debug = True)
+	app.run(debug=True)
